c74d22583ad10558a87c768fe4b37e0d8be5c85f
Reethik Prasad <reethikprasad@gmail.com>
2020-10-31 00:38:26 +0530

    es6

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,26 @@
+// function square(x) {
+//   return x * x;
+// };
+
+// console.log(square(3));
+
+// // const squareArrow = (x) => {
+// //   return x * x;
+// // };
+
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(4));
+
+// Challenge - Use arrow functions
+// getFirstName('Mike Smith') -> "Mike"
+// Create regular arrow function
+// Create arrow function using shorthand syntax
+
+const GetName = (x) => {
+  return x.split(" ")[0];
+};
+
+console.log(GetName("reethik prasad"));
+
+const getName = (x) => x.split(" ")[0];
