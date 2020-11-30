1d7aa8efa5fb5bc88f7a310fb2eaab8fb94c632e
Reethik Prasad <reethikprasad@gmail.com>
2020-11-30 12:30:03 +0530

    bug solved

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
-const path = require("path");
+const path = require('path');
+
+console.log(path.join());
 
 module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
+    entry: [
+        './src/app.js'
     ],
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-  },
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/, 
+            exclude: /node_modules/
+        },
+    {
+        test: /\.s?css$/,
+        use: [
+            'style-loader', 
+            'css-loader',
+            'sass-loader'
+        ]
+    }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
 };
